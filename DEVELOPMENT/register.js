4e5579c9d9dfc725a1076cb763c13cc3c3ffd362
SiemenGijbels <gijbelssiemen@gmail.com>
2015-05-21 12:13:03 +0200

    woiii

---
+++ DEVELOPMENT/register.js
@@ -0,0 +1,23 @@
+$(function(){
+    
+    function init(){
+        
+        $('article div.slider_container').on('click',function(){
+            
+            if($(this).find('.slider').hasClass('on')){
+                $(this).find('.slider').removeClass('on');
+                
+                $('article form .gender').val('V');
+                
+            }else{
+                $('article form .gender').val('M');
+                $(this).find('.slider').addClass('on');
+            }
+            
+        });
+        
+    }
+    
+    init();
+    
+});
