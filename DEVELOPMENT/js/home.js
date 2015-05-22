a95869da791c7e6a9a0e81cce1af2c4c32bbdaf1
Rachouan <rachouan_15@hotmail.fr>
2015-05-22 11:58:31 +0200

    update

---
+++ DEVELOPMENT/js/home.js
@@ -0,0 +1,31 @@
+$(function  () {
+
+	function init () {
+
+		$("article section .info").on("click",function (e) {
+
+			e.preventDefault();
+
+			console.log("clicked");
+
+			var currentId = $(this).attr("id");
+			
+			showDetail(currentId);
+			
+		});
+
+		$(".detail header h1 .back").on("click",function () {
+			$(".detail").addClass("close");
+		});
+	}
+
+	init();
+
+	function showDetail (id) {
+
+		console.log(id);
+
+		$(".detail").removeClass("close");
+	}
+
+});
