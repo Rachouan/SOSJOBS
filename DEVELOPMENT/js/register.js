4f17f739ae5264768f57923e536af2f502211179
SiemenGijbels <gijbelssiemen@gmail.com>
2015-05-22 15:35:21 +0200

    fas

--- DEVELOPMENT/js/register.js
+++ DEVELOPMENT/js/register.js
@@ -1,23 +1,29 @@
-$(function(){
-    
-    function init(){
-        
-        $('article div.slider_container').on('click',function(){
-            
-            if($(this).find('.slider').hasClass('on')){
+$(function () {
+
+    function init() {
+
+        $('article div.slider_container').on('click', function () {
+
+            if ($(this).find('.slider').hasClass('on')) {
                 $(this).find('.slider').removeClass('on');
-                
+
                 $('article form .gender').val('V');
-                
-            }else{
+
+            } else {
                 $('article form .gender').val('M');
                 $(this).find('.slider').addClass('on');
             }
-            
+
         });
-        
+
     }
-    
+
     init();
-    
+
+});
+
+$(function () {
+    $("#accordion").accordion({
+        collapsible: true
+    });
 });
