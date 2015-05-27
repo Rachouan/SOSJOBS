f79a4af4a89ec59e72b7a4892b649805c89b1898
Rachouan <rachouan_15@hotmail.fr>
2015-05-27 09:59:36 +0200

    update

--- DEVELOPMENT/js/profile.js
+++ DEVELOPMENT/js/profile.js
@@ -1,13 +1,15 @@
+var user = JSON.parse(localStorage.getItem("user"));
+
+console.log($(user).length);
+
+if($(user).length > 0){
+
 $(function  () {
 
 
 	function init () {
 		
-		var user = JSON.parse(localStorage.getItem("user"));
-
-		console.log($(user).length);
-
-		if($(user).length > 0){
+		
 
 			$('.profile header').addClass("color"+user.color_code);
 			$('.profile header h1 span.naam').text(user.name+" "+user.last_name);
@@ -49,10 +51,14 @@ $(function  () {
             	});
             });
 
-		}
+		
 
 	}
 
 
 	init();
 });
+
+
+
+}
